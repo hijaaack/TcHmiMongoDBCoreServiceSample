2b88842a68fee7b3ed95c9d3bfceff302cb980c1
Jack Borelius <75740551+hijaaack@users.noreply.github.com>
2023-04-25 15:09:15 +0200

    added clear events button

---
+++ TcHmiProject/TcHmiProject/JS/ClearSqliteLogger.js
@@ -0,0 +1,23 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject;
+        (function (TcHmiProject) {
+            function ClearSqliteLogger() {
+
+                var sendFunction = TcHmi.Functions.getFunction('CallServerFunction');
+
+                if (sendFunction) {
+                    //call the server request function
+                    sendFunction('TcHmiSqliteLogger.Clear', null);
+                }
+
+            }
+            TcHmiProject.ClearSqliteLogger = ClearSqliteLogger;
+        })(TcHmiProject = Functions.TcHmiProject || (Functions.TcHmiProject = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('ClearSqliteLogger', 'TcHmi.Functions.TcHmiProject', TcHmi.Functions.TcHmiProject.ClearSqliteLogger);
